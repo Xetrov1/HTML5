6d13c307cb1f1ae2d021b1355230a6f0c2c7ffd3
Unknown <chandler.g484@stu.nebo.edu>
2019-02-15 10:27:10 -0700

    yay games is fixed
    YAAAAAAAAAAAAAY

--- Adventure/game.js
+++ Adventure/game.js
@@ -1,12 +1,13 @@
-/*var pc = prompt("Its been awhile since i've seen anyone out here, What is your name lost one?");*/
+alert("testing");//var pc = prompt("Its been awhile since i've seen anyone out here, What is your name lost one?");
+Game();
 function Game() {
     
-    var gameName = ["The Gate","The Wall","The ∞-'¢¡"]
-    alert("Welcome to "+gameName[random(0,2)+"!"]);
+    var gameName = ["The Gate","The Wall","The ∞-'¢¡"];
+    alert("Welcome to "+gameName[1]);
     var name = prompt("Hello there mind telling your name before you go into cryostasis").toLowerCase();
     Facility();
 }
-
+/*
 var Player = {
  hp = 20; // out 0f 10
  var mentalhp = 20; // out of 10
@@ -23,39 +24,38 @@ var playerInjuries = {
     rightLeg:0,
     leftFoot:0,
     rightFoot:0;
-    
+
     
 }
+*/
+
 var zombielvl = 1; // out of 100(affects zombies hp and regen and affects there mutation rate)
-/*
+
 var health = CalculateOverallHealth();
-function Game() {
-    alert("The Gate!");    
-    var playerName = prompt("What is your name?");
-    
-    alert("Welcome to The Gate "+playerName);
 
-    Facility();
-}
-
-function Facility() {
-    var awake = prompt("As you wake up you hear a cold robotic voice say Cyropod Deactivated...Running dianostics..Health-good...Hunger 69%...Thirst 40%...Unidentified Energy Fluxuations Initilizing Scan..You slowly open your eyes and wipe away a cold liquid from them before you hear the voice from the outside of the pod you are in...Sector 47 is 67% infected by unknown infection. /n - Look Around, /n - Open Door, /n - Wait").toLowerCase();
-    if (awake == "wait") {
-        var wait = prompt("As you sit there and wait you start to get bored before you see a man across from you in a pod pull the release latch and the door swings down and exits. He looks around before locking eyes with you and asks if you want to leave with him. /n - Open the door, /n - Refuse, /n - Wait").toLowerCase();
-        if (wait == "wait") {
+function Facility() {        ``
+   thing = prompt("As you wake up you hear a cold robotic voice say Cyropod Deactivated...Running dianostics..Health-good...Hunger 69%...Thirst 40%...Unidentified Energy Fluxuations Initilizing Scan..You slowly open your eyes and wipe away a cold liquid from them after you hear the mechanical voice from the outside of the pod...Sector 47 is 67% infected by unknown infection. /n - Look Around, /n - Open Door, /n - Wait") ;
+     switch (thing)
+         
+         case "start":
+        prompt("As you wake up you hear a cold robotic voice say Cyropod Deactivated...Running dianostics..Health-good...Hunger 69%...Thirst 40%...Unidentified Energy Fluxuations Initilizing Scan..You slowly open your eyes and wipe away a cold liquid from them before you hear the voice from the outside of the pod you are in...Sector 47 is 67% infected by unknown infection. /n - Look Around, /n - Open Door, /n - Wait"); break;
+         case "wait":
+   var wait = prompt("As you sit there and wait you start to get bored before you see a man across from you in a pod pull the release latch and the door swings down and exits. He looks around before locking eyes with you and asks if you want to leave with him. /n - Open the door, /n - Refuse, /n - Wait").toLowerCase(); break;
+           case "wait"" {
             var ait = prompt("As you just sit there thinking you hear a low murmur and look out of the pod. The man is staring at someone who is coming close. Hey do you want to leave with me?!, Oh god. You see a women who's flesh is rotting off rush and grab the man and takes a bite of his right arm before your eyes. The man then is screaming and the girl bites his head off. /n - open the door, /n - crouch down");  mentalhp--; // reduces mental hp by 1
-            if (ait == "crouch down") {
+            /*if (ait == "crouch down") {
                 
             }
         }
-    }s
     else if (awake == "Look" || awake == "look around"){
-        var look = prompt("outside of the pod you see that most of the pods vital signs are all straight lines, exepect for three of them. It is also a little dark outside of the pod and you notice its very quiet before you hear a loud boom and see the pod next to you in line break open and you see a young girl come out and you hear moaning and see on the left a female lady come out though its looks like her flesh is rotting off and is slowly drawing close to the crying girl . /n - open door, / n - wait");
+        var look = prompt("outside of the pod you see that most of the 
+        pods vital signs are all straight lines, exepect for three of them. It is also a little dark outside of the pod and you notice its very quiet before you hear a loud boom and see the pod next to you in line break open and you see a young girl come out and you hear moaning and see on the left a female lady come out though its looks like her flesh is rotting off and is slowly drawing close to the crying girl . /n - open door, / n - wait");
     }
     else {
         prompt("As you pull down the door you see a man across from you open his pod door. ");
     }
-}
+    
+    } // end function Facility 
 
 function Stats(hp,mentalhp,viruslvl) {
  var virustest;
@@ -84,7 +84,8 @@ function Stats(hp,mentalhp,viruslvl) {
         }
  var overallhealth = (hp / 2) + (mentalhp / 2) + virustest;
     alert("Overall Health is at: "+ overallhealth);
-} */
+} 
+/*
 var inventory = {
     painkillers:0, // increases health
     ammo:0, // how many shots you have
@@ -98,9 +99,10 @@ var inventory = {
     book:0, // can restore mental health
     money:0, // can use to start a fire 
     lighter:0, // can use to catch something flamable on fires
-  
     
 }
+*/
+/*
 var zombielvl1 = {
     ehp = 10; // zombies hp
     speed = .50; // how fast the zombie moves / attacks
@@ -109,9 +111,11 @@ var zombielvl1 = {
     arms:random(0,2), // zombie cant attack with arms if 0
     legs:random(0,2), // zombie can't move very fast with out legs and must stand still to attack if 0
 }
+*/
+/*
 function Inventory() {
-    var Stuff = prompt(" - take, - use, - take, - what ").toLowerCase();
-    switch(inventory).
+    var inventory = prompt(" - take, - use, - take, - what ").toLowerCase();
+    switch(inventory)
         case "take":
         prompt("What do you want to take? ").toLowerCase();
         break;
@@ -123,10 +127,10 @@ function Inventory() {
         break;
         case "what":
         var item = prompt("What item do you want to know about? ").toLowerCase();
-        switch (item) 
-        case "painkillers": {
+        switch (item) {
+        case "painkillers":
                 alert("Painkillers can give an increase in hp and or add hp to the value.");
-        } break;
+        break;
     case "ammo": alert("Ammo is used to fire firearms or trade for food and such."); break;
     case "knife": alert("Knife is used to harvest food from animals skin or flesh and also to sever zombie's arms and legs and also the head."); break;
     case "huntingRifle":  alert("The Hunting Rifle is used to hunt either animals or zombies or other people, a hunting rifle uses 2 ammo per shot and has farther range than the pistol."); break;
@@ -136,13 +140,16 @@ function Inventory() {
     case "Water":  alert("Water is a basic necesity you can choose when to drink it or have it auto be done."); break;
     case "food": 
     case "Food": alert("Food is a basic necesity you can choose when to eat it or have it auto be done."); break;        
-    case "antidote": || case "Antidote": {
+    case "antidote":
+    case "Antidote":
         alert("Antidotes your best friend they can help fight off the infection if you get bitten or inflicted by any infected thing ");
-    } break;
-    case "book": || case "B": {alert("Ah a book your friend to take a rest from all the crazy mayhem it will restore mental health");
-    
-    } break;
-
+    break;
+    case "book":
+    case "B":
+                alert("Ah a book your friend to take a rest from all the crazy mayhem it will restore mental health");
+    break;
+              }
+*/
 
 
 
